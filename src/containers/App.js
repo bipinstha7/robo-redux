894c064ccbdb4ca54e6f3f67d5bb7a2232766ca6
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-11 11:02:57 +0545

    add connect

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,12 +5,27 @@ import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css"
 
+import {setSearchField} from "../actions";
+import {connect} from "react-redux";
+
+// mapStateToProps takes state and return an object
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value))
+  };
+};
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: [], // map works on array
-      searchfield: ""
+      robots: [] // map works on array
     }
   }
 
@@ -20,22 +35,16 @@ class App extends Component {
       .then(users => this.setState({robots: users}));
   }
 
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value});
-    // console.log(event.target.value);
-    
-  }
-
   render () {
     const filterRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(this.props.searchField.toLowerCase());
     });
     return !this.state.robots.length ? <h1>Loading</h1>
     :
     (
       <div className="tc">
         <h1 className="f2">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={this.props.onSearchChange}/>
         <Scroll>
           <ErrorBoundary>
             <CardList robots={filterRobots}/>
@@ -46,4 +55,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
