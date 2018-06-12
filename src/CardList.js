57c8743c952b66b9c2d27f8772f891e0be399b5a
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-12 23:23:10 +0545

    update props and pass to card

--- src/CardList.js
+++ src/CardList.js
@@ -1,8 +1,21 @@
 import React from "react";
+import Card from "./Card";
 
-const CardList = () => {
+const CardList = ({robots}) => {
+  
   return (
-    
+    <div>
+      {
+        robots.map(robot => {
+          return <Card
+                    key = {robot.id}
+                    id = {robot.id}
+                    name = {robot.name}
+                    email = {robot.email} 
+                 />
+        })
+      }
+    </div>
   )
 }
 
