b5e06e26c04fbf3299a8b8f7dfdf7989cc5211bc
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-12 23:22:30 +0545

    update dynamic content

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({id, name, email}) => {
   return (
-    <div className="bg-light-green div br3 pa3 ma2 grow bw2 shadow-5">
-      <h1>RoboFriends</h1>
-      <img src="https://robohash.org/test?size=200x200" alt="Robot" />
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${id}?size=200x200`} alt="Robot" />
       <div>
-        <h2>John Doe</h2>
-        <p>john.doe@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   )
