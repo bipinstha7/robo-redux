fd9715a193ba76d7042ca5695585ea9370cc06a4
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-12 21:51:58 +0545

    add Card component

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <h1>RoboFriends</h1>
+      <img src="https://robohash.org/test?size=200x200" alt="Robot"/>
+      <div>
+        <h2>John Doe</h2>
+        <p>john.doe@gmail.com</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card';
+import registerServiceWorker from './registerServiceWorker';
+import "tachyons";
+
+ReactDOM.render(<Card />, document.getElementById('root'));
+registerServiceWorker();
+ 
