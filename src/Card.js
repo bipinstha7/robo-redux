ef95e894ca0981592ffb7f89197e2a2fbdd836c3
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-12 22:24:06 +0545

    add tachyons

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div>
+    <div className="bg-light-green div br3 pa3 ma2 grow bw2 shadow-5 ">
       <h1>RoboFriends</h1>
-      <img src="https://robohash.org/test?size=200x200" alt="Robot"/>
+      <img src="https://robohash.org/test?size=200x200" alt="Robot" />
       <div>
         <h2>John Doe</h2>
         <p>john.doe@gmail.com</p>
