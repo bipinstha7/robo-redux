fc1428a0a246680b015e9c878e198c1a33439b32
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-12 23:23:41 +0545

    update app component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import "tachyons";
 
-ReactDOM.render(<CardList />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
