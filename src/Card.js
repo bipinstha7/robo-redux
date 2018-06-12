0b8c99309f3f1d1d4c18ead31310f8abc32cee1e
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-12 22:25:16 +0545

    add CardList component

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div className="bg-light-green div br3 pa3 ma2 grow bw2 shadow-5 ">
+    <div className="bg-light-green div br3 pa3 ma2 grow bw2 shadow-5">
       <h1>RoboFriends</h1>
       <img src="https://robohash.org/test?size=200x200" alt="Robot" />
       <div>
